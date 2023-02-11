f0a793c50748ca3b3d954755a842ddce8c34b274
xzoom117 <amanwhohave@gmail.com>
2023-02-11 14:26:37 +0700

    initial

---
+++ Main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void IsiRak(char Rak[], int jml)
+{
+    for (size_t i = 0; i < jml; i++)
+    {
+        Rak[i] = 'A' + i;
+    }
+}
+
+void MakeNULL(char Rak[])
+{
+    for (size_t i = 0; i < sizeof(Rak); i++)
+    {
+        Rak[i] = '\0';
+    }
+}
+
+// HASNA
+
+// HASNA
+
+// ARYO
+
+// ARYO
+
+// FARHAN
+
+// FARHAN
+
+// ROHIID
+
+// ROHIID
+
+int main(int argc, char *argv[])
+{
+
+    char Rak[20];
+
+    return 0;
+}
